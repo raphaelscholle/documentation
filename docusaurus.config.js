07c1e2368a20d22293b2d611f7c5157820adf5e4
akgnah <1024@setq.me>
2022-11-29 15:53:06 +0800

    add i18n demo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,8 +23,17 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh',
+    locales: ['en', 'zh'],
+    path: 'i18n',
+    localeConfigs: {
+      en: {
+        htmlLang: 'en-US',
+      },
+      zh: {
+        htmlLang: 'zh-CN',
+      }
+    },
   },
 
   presets: [
@@ -73,6 +82,10 @@ const config = {
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
+            position: 'right',
+          },
+          {
+            type: 'localeDropdown',
             position: 'right',
           },
         ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Translate from '@docusaurus/Translate';
 
 import styles from './index.module.css';
 
@@ -12,6 +13,9 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
+        <h1>
+          <Translate id="homepage.hero.hello">你好，世界！</Translate>
+        </h1>
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
